c631c698c97cf73837d16b3793eb83ce3f0d0b1d
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2021-02-10 21:48:21 +0800

    option and element design

--- src/elements/fig.rs
+++ src/elements/fig.rs
@@ -4,33 +4,57 @@ use crate::color::RGBColor;
 #[allow(dead_code)]
 pub struct FigElm {
     background_color: RGBColor,
+    border: f32,
 }
-
-#[derive(Clone, Copy)]
-pub enum FigureOption {
-    BackGroundColor(RGBColor),
-    Border(f32),
-}
-
-impl Option for FigureOption {}
 
 impl Element for FigElm {
     #[allow(unused_variables)]
-    fn parse_options<T: Option>(&self, options: Vec<T>) -> Self
+    fn parse_options(&mut self, options: Vec<impl Option<Self>>) -> &Self
     where
         Self: Sized,
     {
-        return FigElm {
-            background_color: RGBColor(0, 0, 0),
-        };
+        for o in options {
+            o.match_elm_param(self);
+        }
+
+        return self;
     }
 }
 
 impl FigElm {
     pub fn new(options: Vec<FigureOption>) -> Self {
-        FigElm {
+        let mut fig = FigElm {
             background_color: RGBColor(0, 0, 0),
+            border: 0.0,
+        };
+        fig.parse_options(options);
+        fig
+    }
+
+    pub fn set_background_color(&mut self, color: RGBColor) {
+        self.background_color = color;
+    }
+
+    pub fn set_border(&mut self, border: f32) {
+        self.border = border;
+    }
+}
+
+// #[derive(Clone, Copy)]
+pub enum FigureOption {
+    BackGroundColor(RGBColor),
+    Border(f32),
+}
+
+// impl FigureOption {
+//     fn _match_elm_param(&self, fig_elm: &mut FigElm) {}
+// }
+
+impl Option<FigElm> for FigureOption {
+    fn match_elm_param(&self, fig_elm: &mut FigElm) {
+        match self {
+            FigureOption::BackGroundColor(c) => fig_elm.set_background_color(*c),
+            FigureOption::Border(pct) => fig_elm.set_border(*pct),
         }
-        .parse_options(options)
     }
 }
--- src/elements/mod.rs
+++ src/elements/mod.rs
@@ -1,9 +1,11 @@
 pub mod fig;
 
 pub trait Element {
-    fn parse_options<T: Option>(&self, options: Vec<T>) -> Self
+    fn parse_options(&mut self, options: Vec<impl Option<Self>>) -> &Self
     where
         Self: Sized;
 }
 
-pub trait Option {}
+pub trait Option<T> {
+    fn match_elm_param(&self, elm: &mut T);
+}
